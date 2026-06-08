eac7c5fcab9f5d4fe7f0d0b9192f9a94160903a2
hoangtuzami <hoangtuzami@gmail.com>
2026-06-08 14:32:23 +0700

    Add D-14/D-7/D-3/D-0 demo milestone scenarios

--- src/main/java/com/isums/contractservice/domains/enums/ContractDemoScenario.java
+++ src/main/java/com/isums/contractservice/domains/enums/ContractDemoScenario.java
@@ -3,7 +3,11 @@ package com.isums.contractservice.domains.enums;
 public enum ContractDemoScenario {
     D60(60),
     D30(30),
+    D14(14),
+    D7(7),
+    D3(3),
     D1(1),
+    D0(0),
     EXPIRED(-1),
     CUSTOM(null);
 
